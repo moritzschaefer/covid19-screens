59adecc3ace9c422b5486189fcece3f447d97666
Moritz Schaefer <mail@moritzs.de>
2021-04-10 14:24:36 +0000

    update wei

--- wei/README.txt
+++ wei/README.txt
@@ -1,5 +1,5 @@
 Wei et al. don't provide raw reads of their screens. They only provide read count tables for their sgRNAs. 
 
-Instead of filtering out duplicate sgRNAs (5017), I merged their targets, as I did for our own Vero-data. Further, based on the sgRNA-sequences, I identified the ENSEMBL IDs for ~1000 genes, which had generic LOC-gene-ids and replaced them
+Instead of filtering out duplicate sgRNAs (5017), I merged their targets, as I did for our own Vero-data. Further, based on the sgRNA-sequences, I identified the ENSEMBL IDs for ~1000 genes, which had generic LOC-gene-ids and replaced them.
 
-Differently from the other folders, the noramlized readcounts are also to be found in the test/ directory
+
--- wei/match_ids.py
+++ wei/match_ids.py
@@ -10,12 +10,15 @@ def parse_gene_name(description):
     except (IndexError, TypeError, AttributeError):
         return re.search('gene:(ENSCSAG[0-9.]+) ', description).groups()[0]
         
-x=SeqIO.parse('./Chlorocebus_sabaeus.ChlSab1.1.cdna.abinitio.fa', 'fasta')
+x=SeqIO.parse('./Chlorocebus_sabaeus.ChlSab1.1.cdna.all.fa', 'fasta')
 
-import pdb; pdb.set_trace()
-seqs = {s.name: s.seq for s in x}
+seqs = {parse_gene_name(s.description): s.seq for s in x}
     
 merged_counts = pd.read_csv('./sgrna_counts_merged.tsv', sep='\t')
 matches = merged_counts['sgRNA'].apply(lambda sg: [gene for gene, seq in seqs.items() if re.search(f'{sg}.GG', str(seq)) or re.search(f'{sg}.GG', str(seq.reverse_complement()))])
 merged_counts['new_gene'] = matches.apply(lambda row: re.match('[^.]+', row[0]).group() if len(row) == 1 else (None if len(row) == 0 else f'multiKO_{"_".join([re.match("[^.]+", v).group() for v in row])}'))
+merged_counts.to_csv('merged_counts_with_ensembl_mapping.csv', index=False)
+merged_counts['gene'] = merged_counts.apply(lambda row: row['new_gene'] if row['gene'].startswith('LOC') and row['new_gene'] != None else row['gene'], axis=1)
+merged_counts.columns = [s.replace(' ', '_') for s in merged_counts.columns
+merged_counts.drop(['new_gene'], axis=1).to_csv('merged_counts_with_replaced_ensembl_mapping.tsv', sep='\t', index=False)
 
